4f5b1cd00bc1bf4ed8f325c3569d727700f428fb
Bruno Mota <bmotadev@gmail.com>
2023-02-21 17:13:53 -0300

    :sparkles: feat: hoisting

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+let animal = 'Gato';
+
+function sayAnimal() {
+    console.log(animal);
+}
+// o hoisting é o ato do interpretador do javascript jogar as variaveis e funções para cima do codigo. Como evitar isso: usar let e const
+sayAnimal();
+
+
